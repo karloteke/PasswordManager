717cb1b95c076d24de4dc70ab227bdfa3ba0ae06
Carlota Cetina <carlotika26@gmail.com>
2023-12-09 12:52:52 +0100

    Insert category

--- www/category.js
+++ www/category.js
@@ -1,3 +1,16 @@
+let drawCategories = (data) => {
+  let parent = document.getElementById('categoryList');
+  data.forEach(category => {
+    let child = document.createElement('a');
+    child.className = "list-group-item list-group-item-action";
+    child.innerText = category.name;
+    parent.appendChild(child);
+  });
+}
+
+fetch("http://localhost:3000/categories")
+  .then(res => res.json())
+  .then(data => drawCategories(data))
 
 function openModal() {
     document.getElementById("myModal").classList.add("active");
@@ -17,3 +30,30 @@ function openModal() {
       insertCategory(value)
     }
   }
+
+  function insertCategory(categoryName) {
+    const apiUrl = 'http://localhost:3000/categories';
+    const data = {
+        name: categoryName
+    };
+
+    // Cabecera petición
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify(data), // Convierte el objeto JavaScript a JSON
+    };
+
+    fetch(apiUrl, requestOptions)
+        .then(response => response.json()) 
+        .then(data => {
+            console.log('Respuesta del servidor:', data);
+            closeModal();
+            window.location.reload();
+        })
+        .catch(error => {
+            console.error('Error en la petición:', error);
+        });
+}
